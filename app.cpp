4cb1e137cbe20034f08981f9282e91ba442aad48
Harsh Pandhe <pandhemihir@gmail.com>
2024-08-06 16:57:19 +0530

    Update 05

--- app.cpp
+++ app.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
@@ -12,10 +13,17 @@ struct Order
     double price;
 };
 
+struct MenuItem
+{
+    string name;
+    double price;
+};
+
 class RestaurantBillingSystem
 {
 private:
     vector<Order> orders;
+    vector<MenuItem> menu;
 
 public:
     void displayMenu()
@@ -24,7 +32,11 @@ public:
         cout << "1. Enter Order" << endl;
         cout << "2. View Orders" << endl;
         cout << "3. Calculate Bill" << endl;
-        cout << "4. Exit" << endl;
+        cout << "4. Add Menu Item" << endl;
+        cout << "5. Edit Menu Item" << endl;
+        cout << "6. Remove Menu Item" << endl;
+        cout << "7. Save Menu to File" << endl;
+        cout << "8. Exit" << endl;
         cout << "------------------------------" << endl;
     }
 
@@ -38,10 +50,28 @@ public:
         getline(cin, item);
         cout << "Enter quantity: ";
         cin >> quantity;
-        cout << "Enter price: ";
-        cin >> price;
-        orders.push_back({item, quantity, price});
-        cout << "Order entered successfully." << endl;
+        price = getItemPrice(item);
+        if (price != -1)
+        {
+            orders.push_back({item, quantity, price});
+            cout << "Order entered successfully." << endl;
+        }
+        else
+        {
+            cout << "Item not found in menu." << endl;
+        }
+    }
+
+    double getItemPrice(const string &itemName)
+    {
+        for (const auto &menuItem : menu)
+        {
+            if (menuItem.name == itemName)
+            {
+                return menuItem.price;
+            }
+        }
+        return -1;
     }
 
     void viewOrders()
@@ -79,6 +109,74 @@ public:
         }
     }
 
+    void addMenuItem()
+    {
+        string name;
+        double price;
+        cout << "Enter item name: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Enter price: ";
+        cin >> price;
+        menu.push_back({name, price});
+        cout << "Menu item added successfully." << endl;
+    }
+
+    void editMenuItem()
+    {
+        string name;
+        cout << "Enter item name to edit: ";
+        cin.ignore();
+        getline(cin, name);
+        for (auto &menuItem : menu)
+        {
+            if (menuItem.name == name)
+            {
+                cout << "Enter new price: ";
+                cin >> menuItem.price;
+                cout << "Menu item updated successfully." << endl;
+                return;
+            }
+        }
+        cout << "Item not found in menu." << endl;
+    }
+
+    void removeMenuItem()
+    {
+        string name;
+        cout << "Enter item name to remove: ";
+        cin.ignore();
+        getline(cin, name);
+        for (auto it = menu.begin(); it != menu.end(); ++it)
+        {
+            if (it->name == name)
+            {
+                menu.erase(it);
+                cout << "Menu item removed successfully." << endl;
+                return;
+            }
+        }
+        cout << "Item not found in menu." << endl;
+    }
+
+    void saveMenuToFile()
+    {
+        ofstream file("menu.txt");
+        if (file.is_open())
+        {
+            for (const auto &menuItem : menu)
+            {
+                file << menuItem.name << "," << menuItem.price << endl;
+            }
+            file.close();
+            cout << "Menu saved to file successfully." << endl;
+        }
+        else
+        {
+            cout << "Error opening file." << endl;
+        }
+    }
+
     void exitSystem()
     {
         cout << "Exiting system..." << endl;
@@ -105,6 +203,18 @@ public:
                 calculateBill();
                 break;
             case 4:
+                addMenuItem();
+                break;
+            case 5:
+                editMenuItem();
+                break;
+            case 6:
+                removeMenuItem();
+                break;
+            case 7:
+                saveMenuToFile();
+                break;
+            case 8:
                 exitSystem();
                 break;
             default:
